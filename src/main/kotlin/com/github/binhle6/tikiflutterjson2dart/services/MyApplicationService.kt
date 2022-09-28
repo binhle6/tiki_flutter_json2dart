4096f647e8f8e5d36a7512a40df4c0c01ba66c86
binhle6 <binh.le6@tiki.vn>
2022-09-28 10:47:03 +0700

    delete unused component

--- src/main/kotlin/com/github/binhle6/tikiflutterjson2dart/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/binhle6/tikiflutterjson2dart/services/MyApplicationService.kt
@@ -1,13 +1,13 @@
 package com.github.binhle6.tikiflutterjson2dart.services
 
-import com.github.binhle6.tikiflutterjson2dart.MyBundle
+//import com.github.binhle6.tikiflutterjson2dart.MyBundle
 
-class MyApplicationService {
-
-    init {
-        println(MyBundle.message("applicationService"))
-
-        System.getenv("CI")
-            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
-}
+//class MyApplicationService {
+//
+//    init {
+//        println(MyBundle.message("applicationService"))
+//
+//        System.getenv("CI")
+//            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+//    }
+//}
--- src/main/kotlin/com/github/binhle6/tikiflutterjson2dart/services/MyProjectService.kt
+++ src/main/kotlin/com/github/binhle6/tikiflutterjson2dart/services/MyProjectService.kt
@@ -1,14 +1,14 @@
 package com.github.binhle6.tikiflutterjson2dart.services
 
-import com.intellij.openapi.project.Project
-import com.github.binhle6.tikiflutterjson2dart.MyBundle
+//import com.intellij.openapi.project.Project
+//import com.github.binhle6.tikiflutterjson2dart.MyBundle
 
-class MyProjectService(project: Project) {
-
-    init {
-        println(MyBundle.message("projectService", project.name))
-
-        System.getenv("CI")
-            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
-}
+//class MyProjectService(project: Project) {
+//
+//    init {
+//        println(MyBundle.message("projectService", project.name))
+//
+//        System.getenv("CI")
+//            ?: TODO("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+//    }
+//}
